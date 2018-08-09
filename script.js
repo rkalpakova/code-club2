65f877598e946f18d3deceee8d18d03cd32858e4
rkalpakova <42213213+rkalpakova@users.noreply.github.com>
2018-08-09 12:48:03 +0100

    My first commit, Yay!

---
+++ script.js
@@ -0,0 +1,40 @@
+// Add your code below...
+const myName = "Radina"
+const mySurname = "Kalpakova"
+const myFullName = myName + " " + mySurname
+
+const number1 = 2
+const number2 = 8
+const sumNumbers = number1 + number2
+
+function flipCoin(threshold) {
+	const randomNumber = Math.random();
+	if (randomNumber > threshold) {
+		alert("You won, " + myFullName);
+	} else {
+		alert("You lost, " + myFullName);
+	}
+}
+
+function resizeButton() {
+	const newRadius = Math.random() * 20;
+	$('.button').css('border-radius', newRadius + 'px');
+}
+
+function slideImage() {
+	$('.myProfile').slideUp();
+	$('.myProfile').slideDown();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
